41dd3de57572911eb0854b67bcaa8dd2f06720ce
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-02 16:56:32 +0100

    Since a species in the eventual solution will probably just have a name, we'll use species instead of reaction system for the first class

--- reactor/reactor.cpp
+++ reactor/reactor.cpp
@@ -1,7 +1,7 @@
 // We write comments like this
 
 #include <iostream> //Include standard library file which provides input and output capabilities ("print statements") 
-#include "ReactionSystem.h" // Include our own library file for describing a reaction system
+#include "Species.h" // Include our own library file for describing a reaction system
 
 int main(int argument_count, char ** command_line_arguments) // Define the main function, which is the entry point to the program.
 { // C++ uses braces to start and end sections of code.
@@ -9,10 +9,10 @@ int main(int argument_count, char ** command_line_arguments) // Define the main 
   std::cout << "The 'boiler plate' main program is running." << std::endl; // Print out a friendly message.
   // The << symbol is an output operator, while std::cout tells the message to go to "standard output", i.e. your terminal.
   
-  ReactionSystem my_simple_reaction_system("Simple reaction system"); // You can declare new variables like this.
-  // "ReactionSystem" is a user defined type, or "class".
+  Species calcium("Ca"); // You can declare new variables like this.
+  // "Species" is a user defined type, or "class".
   
-  std::cout << "A reaction system was created called \"" << my_simple_reaction_system.GetName() << " \"" << std::endl; // You use a dot to access a method of a class.
+  std::cout << "A species was created called \"" << calcium.GetName() << "\"" << std::endl; // You use a dot to access a method of a class.
   // To get a quote symbol into a string, escape it with a slash
   // << can be used to chain several outputs together, in this case, a literal string,
   // and the output from calling GetName()
--- reactor/src/Species.cpp
+++ reactor/src/Species.cpp
@@ -1,12 +1,12 @@
-#include "ReactionSystem.h" // Include the declaration of the ReactionSystem..
+#include "Species.h" // Include the declaration of the Species..
 
-ReactionSystem::ReactionSystem(const std::string &input_name):
+Species::Species(const std::string &input_name):
     name(input_name) // Initialise the name member variable from the input argument.
   {
     // do nothing
   }
 
-const std::string & ReactionSystem::GetName() 
+const std::string & Species::GetName() 
 { 
 	// a function whose return type is a standard string, with no input arguments
 	// the ampersand and const will be explained later.
