0c74586cbd6dffeb672ead688155e8efff31b587
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-02 16:28:59 +0100

    Once guard

--- reactor/src/ReactionSystem.h
+++ reactor/src/ReactionSystem.h
@@ -1,11 +1,16 @@
 #include <string> // use the string capabilities from the standard library
 
+#ifndef ONCE_REACTION_SYSTEM_H
+#define ONCE_REACTION_SYSTEM_H
+
 class ReactionSystem // A "class" is a user defined type with built-in functions
 {   
 public:
-  ReactionSystem(const std::string &input_name);
-  const std::string & GetName();
+  ReactionSystem(const std::string &input_name); // Declare constructor function.
+  const std::string & GetName(); // Declare a get name function.
 private:
   std::string name; // A member variable for the class to store the name of the system.
 
 };
+
+#endif //ONCE_REACTION_SYSTEM_H
