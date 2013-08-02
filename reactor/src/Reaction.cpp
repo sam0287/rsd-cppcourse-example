8f7c8cc264bf024a0e4f7fad7797eccc46258c91
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-02 17:22:08 +0100

    Replace Species with new class name

--- reactor/src/Reaction.cpp
+++ reactor/src/Reaction.cpp
@@ -1,12 +1,12 @@
-#include "Species.h" // Include the declaration of the Species..
+#include "Reaction.h" // Include the declaration of the Reaction..
 
-Species::Species(const std::string &input_name):
+Reaction::Reaction(const std::string &input_name):
     name(input_name) // Initialise the name member variable from the input argument.
   {
     // do nothing
   }
 
-const std::string & Species::GetName() 
+const std::string & Reaction::GetName() 
 { 
 	// a function whose return type is a standard string, with no input arguments
 	// the ampersand and const will be explained later.
