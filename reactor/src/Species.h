5f96f0978048551edee1e055577155738e6b039f
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-06 15:38:19 +0100

    Species can have a rate of change

--- reactor/src/Species.h
+++ reactor/src/Species.h
@@ -10,11 +10,16 @@ class Species // A "class" is a user defined type with built-in functions
 public:
   Species(const std::string &input_name); // Declare constructor function.
   const std::string & GetName() const;// Declare a get name function.
-  void SetConcentration(double new_concentration) {concentration=new_concentration;} // Set concentration
+  void   SetConcentration(double new_concentration) {concentration=new_concentration;} // Set concentration
   double GetConcentration() const {return concentration;}
+  void   ReSetRateOfChange() {rate_of_change=0;} // Set concentration
+  double GetRateOfChange() const {return rate_of_change;}
+  void	 ContributeToRateOfChange(double flux) {rate_of_change+=flux;}
+
 private:
   std::string name; // A member variable for the class to store the name of the system.
   double concentration;
+  double rate_of_change;
 };
 
 #endif //ONCE_SPECIES_H
--- reactor/test/SpeciesTest.cpp
+++ reactor/test/SpeciesTest.cpp
@@ -21,6 +21,34 @@ TEST(SpeciesTest, CanSetSpeciesConcentration) { // First argument is test group,
   EXPECT_EQ(2.0, mySpecies.GetConcentration()); // Assert that the name should be as expected
 }
 
+// Test that the system has a name as expected.
+TEST(SpeciesTest, SpeciesHasADefaultRateOfChange) { // First argument is test group, second is test name
+  Species mySpecies("SomeName"); // Create a species with a specified name
+  EXPECT_EQ(0, mySpecies.GetRateOfChange()); // Assert that the name should be as expected
+}
+
+// Test that the system has a name as expected.
+TEST(SpeciesTest, CanContributeToSpeciesRateOfChange) { // First argument is test group, second is test name
+  Species mySpecies("SomeName"); // Create a species with a specified name
+  EXPECT_EQ(0, mySpecies.GetConcentration()); // Assert that the name should be as expected
+  mySpecies.ContributeToRateOfChange(2.0);
+  EXPECT_EQ(2.0, mySpecies.GetRateOfChange()); // Assert that the name should be as expected
+
+  mySpecies.ContributeToRateOfChange(3.0);
+  EXPECT_EQ(5.0, mySpecies.GetRateOfChange()); // Assert that the name should be as expected
+}
+
+// Test that the system has a name as expected.
+TEST(SpeciesTest, CanResetRateOfChange) { // First argument is test group, second is test name
+  Species mySpecies("SomeName"); // Create a species with a specified name
+  EXPECT_EQ(0, mySpecies.GetConcentration()); // Assert that the name should be as expected
+  mySpecies.ContributeToRateOfChange(2.0);
+  EXPECT_EQ(2.0, mySpecies.GetRateOfChange()); // Assert that the name should be as expected
+  mySpecies.ReSetRateOfChange();
+  mySpecies.ContributeToRateOfChange(3.0);
+  EXPECT_EQ(3.0, mySpecies.GetRateOfChange()); // Assert that the name should be as expected
+}
+
 int main(int argc, char **argv) { // A main function scaffold to call the tests
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
