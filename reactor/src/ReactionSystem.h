09ccae1b317e5e1a53aea7c3c58220671033ad04
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-07 15:12:09 +0100

    Tidy up and reorder reaction system header

--- reactor/src/ReactionSystem.h
+++ reactor/src/ReactionSystem.h
@@ -15,18 +15,17 @@ class ReactionSystem // A "class" is a user defined type with built-in functions
 public:
   ReactionSystem(); // Declare constructor function.
   ~ReactionSystem(); // Delete the reaction system
+
   Reaction & NewReaction(double rate);
   const std::vector<Reaction *> & GetReactions() const {return reactions;}
+
+  Species & NewSpecies(const std::string &name);
+  const std::vector<Species *> & GetSpecies() const { return species;}
   
   const std::vector<double> GetRatesOfChange() const; // Given the current species concentrations, return the rates of change
-
   const std::vector<double> GetConcentrations() const ; // return array of species concentrations
-  void SetConcentrations(const std::vector<double> & );
-
-  const std::vector<Species *> & GetSpecies() const { return species;}
-
   void GetRatesGivenConcentrations(const std::vector<double> & concentrations, std::vector<double> & rates);
-  Species & NewSpecies(const std::string &name);
+  void SetConcentrations(const std::vector<double> & );
 
 private:
   ReactionSystem(const ReactionSystem& that); //disable copy
