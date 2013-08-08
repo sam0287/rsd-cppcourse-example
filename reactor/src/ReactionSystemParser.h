a8301d0d4a9e51f9ae72f3a9f750f6a1488ae4f3
James Hetherington <j.hetherington@ucl.ac.uk>
2013-08-08 11:26:05 +0100

    Spacing

--- reactor/src/ReactionSystemParser.h
+++ reactor/src/ReactionSystemParser.h
@@ -16,7 +16,8 @@ public:
   ReactionSystemParser(){};
   ReactionSystem * FromStream(std::istream & source);
   void ParseLine(std::string & source, std::vector<std::string>& reactant_names, std::vector<std::string> & product_names, double & rate);
-  Species * NewOrFind(ReactionSystem * result,const std::string &name);
+  Species * NewOrFind(ReactionSystem * result, const std::string &name);
+  
 private:
   std::map<std::string, Species *> species_map;
 };
