d1df6c9fc112d9770255fa250518f2a6fd7c2ed1
James Hetherington <j.hetherington@ucl.ac.uk>
2013-09-18 18:03:10 +0100

    Add rate of change to Species

--- reactor/src/Species.h
+++ reactor/src/Species.h
@@ -13,6 +13,9 @@ namespace reactor
     const std::string & GetName() const;// Declare a get name function.
     void   SetConcentration(double new_concentration) {concentration=new_concentration;} // Set concentration
     double GetConcentration() const {return concentration;}
+    void   ReSetRateOfChange() {rate_of_change=0;} // Set concentration
+    double GetRateOfChange() const {return rate_of_change;}
+    void	 ContributeToRateOfChange(double flux) {rate_of_change+=flux;}
 
   private:
     std::string name; // A member variable for the class to store the name of the system.
